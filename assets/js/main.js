11b8140363aba2f1b2959e348cf3469e35bc3c68
gorilla-25 <89886828+gorilla-25@users.noreply.github.com>
2021-09-17 01:43:49 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@ app.component('answer-input', {
     return {
       /* 送信ボタン上下に表示されるメッセージ */
       okMessage: '正解！',
-      ngMessage: 'そのキーワードは違うようだぞ！？',
+      ngMessage: 'そのキーワードは違うようだ…。',
       message: '',
       inputAnswer: '',
     }
@@ -108,7 +108,7 @@ app.component('answer-input', {
   template: `
     <div class="answer__container">
       <div class="answer">
-        <input type="text" v-model="inputAnswer" placeholder="ここに答えを入力しよう">
+        <input type="text" v-model="inputAnswer" placeholder="答えをひらがなで入力しよう！">
       </div>
       <p v-if="message === ngMessage" class="err-message">{{ message }}</p>
       <button v-on:click="judgement(inputAnswer)">送信</button>
